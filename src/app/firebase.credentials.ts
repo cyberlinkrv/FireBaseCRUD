c0ffd1e07bf3a55f84d6c02026c523b50510abbd
cyberlinkrv <divino_j_silva@hotmail.com>
2018-04-13 00:49:14 -0300

    Work Add item and List Item

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,10 @@
+export const FIREBASE_ONFIG = {
+    apiKey: "AIzaSyDgHFfgtoP77UumiA5UcQZfB85cIE50qRo",
+    authDomain: "bom-cliente.firebaseapp.com",
+    databaseURL: "https://bom-cliente.firebaseio.com",
+    projectId: "bom-cliente",
+    storageBucket: "bom-cliente.appspot.com",
+    messagingSenderId: "868750817317"
+  };
+
+
